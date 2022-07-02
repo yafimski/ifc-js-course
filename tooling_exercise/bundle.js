919f53697d60097c5afbc994da79454b70a6e9da
Yafim Simanovsky <yafimski@gmail.com>
2022-07-02 21:05:24 +0300

    init tooling exercise

--- tooling_exercise/bundle.js
+++ tooling_exercise/bundle.js
@@ -1,4 +1,30 @@
-import {projects} from "./projects.js";
+const projects = [
+    {
+        name: 'Model 1',
+        id: '1',
+        url: 'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/01'
+    },
+    {
+        name: 'Model 2',
+        id: '2',
+        url: 'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/02'
+    },
+    {
+        name: 'Model 3',
+        id: '3',
+        url: 'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/03'
+    },
+    {
+        name: 'Model 4',
+        id: '4',
+        url: 'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/04'
+    },
+    {
+        name: 'Model 5',
+        id: '5',
+        url: 'https://ifcjs.github.io/ifcjs-crash-course/sample-apps/05'
+    }
+];
 
 const projectContainer = document.getElementById("projects-container");
 const baseURL = './model-viewer.html';
